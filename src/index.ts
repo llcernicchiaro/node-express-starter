2e9c13e1f9f77f9aff434ac49af24f4a262f99ec
Lorenzo Lopes Cernicchiaro <lopescernicchiaro@gmail.com>
2023-06-23 15:34:54 -0300

    adding nodemon and lint-staged files and adding helmet and cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import "dotenv/config";
 import express, { type Express, type Request, type Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
 
+// App Variables
 const app: Express = express();
 const port = process.env.PORT;
+
+// App Configuration
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express - TypeScript Server");
